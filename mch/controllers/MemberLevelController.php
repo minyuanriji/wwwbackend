c68d1406b65a8406e209659a7e159f348fe87bf2
mr.lin <746027209@qq.com>
2021-02-04 15:16:14 +0800

    33

--- mch/controllers/MemberLevelController.php
+++ mch/controllers/MemberLevelController.php
@@ -2,8 +2,20 @@
 namespace app\mch\controllers;
 
 use app\core\ApiCode;
+use app\forms\mall\member\MemberLevelForm;
 
 class MemberLevelController extends MchController {
+
+    /**
+     * 获取所有会员等级
+     * @return \yii\web\Response
+     */
+    public function actionAllMember() {
+        $form = new MemberLevelForm();
+        $res = $form->getAllMemberLevel();
+
+        return $this->asJson($res);
+    }
 
     /**
      * 判断是否有会员卡插件权限

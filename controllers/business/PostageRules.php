b0067a940c76c072720537858688c021aac0f54b
陈志民 <877170716@qq.com>
2021-03-10 15:41:06 +0800

    [*]修改返回的快递运费数据

--- controllers/business/PostageRules.php
+++ controllers/business/PostageRules.php
@@ -5,24 +5,30 @@ use app\models\mysql\Goods;
 class PostageRules{
     public function getExpressPrice($data){
         try {
-            $freight = (new Goods()) -> getGoodsFreightId($data['order_id']);
-            if($freight['mch_id'] > 0){
-                return ['price' => 0];
-            }
-            if($freight['freight_id'] == 0){
-                $result = (new PostageRulesModel()) -> getExpressPrice();
-            }else{
-                $result = (new PostageRulesModel()) -> getGoodsExpressPrice($freight['freight_id']);
-            }
-            $result = json_decode($result -> toArray()['detail'],true);
-            if(array_search($data['data'],array_column($result[0]['list'],'name')) !== false){
-                $price = $result[0]['firstPrice'];
-            }else{
-                $price = $result[1]['firstPrice'];
+            $dataArr = explode(',', $data['order_id']);
+            $data_price = [];
+            foreach ($dataArr as $key => $val) {
+                $freight = (new Goods())->getGoodsFreightId($val);
+                if ($freight['mch_id'] > 0) {
+                    $price = 0;
+                } else {
+                    if ($freight['freight_id'] == 0) {
+                        $result = (new PostageRulesModel())->getExpressPrice();
+                    } else {
+                        $result = (new PostageRulesModel())->getGoodsExpressPrice($freight['freight_id']);
+                    }
+                    $result = json_decode($result->toArray()['detail'], true);
+                    if (array_search($data['data'], array_column($result[0]['list'], 'name')) !== false) {
+                        $price = $result[0]['firstPrice'];
+                    } else {
+                        $price = $result[1]['firstPrice'];
+                    }
+                }
+                $data_price[$val] = $price;
             }
         }catch (\Exception $e){
-            $price = [];
+            $data_price[0] = $e -> getMessage();
         }
-        return ['price' => $price];
+        return $data_price;
     }
 }

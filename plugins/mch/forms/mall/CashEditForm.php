3dd27ac4bd3f382bdeb91b864a8d5b9baf57e749
mr.lin <746027209@qq.com>
2021-03-17 16:43:15 +0800

    #微信打款接口加入日志代码

--- plugins/mch/forms/mall/CashEditForm.php
+++ plugins/mch/forms/mall/CashEditForm.php
@@ -140,15 +140,19 @@ class CashEditForm extends BaseModel
                         throw new \Exception('系统未开启微信支付！');
                     }
 
-                    $res = $payment->transfer->toBalance([
+                    $transferData = [
                         'partner_trade_no'  => $wechatPaySetting['wechat_mch_id'].'x'.$mchCash->id, // 商户订单号，需保持唯一性(只能是字母或者数字，不能包含有符号)
                         'openid'            => $userInfo->openid,
                         'check_name'        => 'NO_CHECK', // NO_CHECK：不校验真实姓名, FORCE_CHECK：强校验真实姓名
                         're_user_name'      => '', // 如果 check_name 设置为FORCE_CHECK，则必填用户真实姓名
                         'amount'            => $mchCash->fact_price * 100, // 企业付款金额，单位为分
                         'desc'              => '收益提现', // 企业付款操作说明信息。必填
-                    ]);
+                    ];
+                    $res = $payment->transfer->toBalance($transferData);
                     if ($res['result_code'] == 'FAIL') {
+                        $errLogPath = \Yii::getAlias('@runtime/') . "/transfer_err.log";
+                        $transferData['res'] = $res;
+                        file_put_contents($errLogPath, json_encode($transferData) . "\n", FILE_APPEND);
                         throw new \Exception($res['err_code_des']);
                     }
 

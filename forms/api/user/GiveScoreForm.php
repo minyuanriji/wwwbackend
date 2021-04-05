555b5d188c56e7db6382009e37e0c68e831f7c92
mr.lin <746027209@qq.com>
2021-04-05 14:39:44 +0800

    #14:39

--- forms/api/user/GiveScoreForm.php
+++ forms/api/user/GiveScoreForm.php
@@ -4,6 +4,7 @@ namespace app\forms\api\user;
 
 use app\core\ApiCode;
 use app\models\BaseModel;
+use app\models\ScoreLog;
 use app\models\User;
 
 class GiveScoreForm extends BaseModel{
@@ -26,14 +27,23 @@ class GiveScoreForm extends BaseModel{
         }
 
         try {
+
+            $exists = ScoreLog::find()->where([
+                "user_id"     => $this->user_id,
+                "source_type" => "new_user"
+            ])->exists();
+            if($exists){
+                throw new \Exception("您已经领取过了~");
+            }
+
             $user = User::findOne($this->user_id);
             if(!$user){
                 throw new \Exception("用户不存在");
             }
-            \Yii::$app->currency->setUser($user)->score->add((int)$this->number, $this->desc, json_encode([]));
+            \Yii::$app->currency->setUser($user)->score->add((int)$this->number, $this->desc, json_encode([]), "new_user");
             return [
                 'code'  => ApiCode::CODE_SUCCESS,
-                'msg'   => '支付成功'
+                'msg'   => '领取成功'
             ];
         }catch (\Exception $e){
             return $this->returnApiResultData(ApiCode::CODE_FAIL, $e->getMessage());

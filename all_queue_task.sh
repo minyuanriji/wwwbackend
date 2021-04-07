58f9e1e130628555e5957e4fab38f8557cb5f4ed
mr.lin <746027209@qq.com>
2021-04-07 09:49:29 +0800

    #队列执行脚本

--- all_queue_task.sh
+++ all_queue_task.sh
@@ -3,14 +3,14 @@
 basepath=$(cd `dirname $0`; pwd)
 chmod a+x "$basepath/yii"
 
-command="/www/server/php/73/bin/php $basepath/yii queue-task/listen"
+command="/www/server/php/73/bin/php $basepath/yii queue-task/execute"
 
 result=$(ps -ef | grep "`echo $command`" | grep -v "grep")
 
 if [ ! -n "$result" ]
 then
   echo "Starting the process."
-  str=$(nohup $command >/dev/null 2>&1 &)
+  str=$(nohup $command >/dev/null &)
   echo -e "\033[32mOk.\033[0m"
 else
   echo "The process has been started."

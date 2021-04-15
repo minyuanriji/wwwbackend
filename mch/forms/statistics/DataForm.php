712d775e2aba83f23ae18c3d1ea0d4dd3ae1e804
wmc <205176371.com>
2021-04-15 18:44:40 +0800

    修改多商户后台概况人数显示

--- mch/forms/statistics/DataForm.php
+++ mch/forms/statistics/DataForm.php
@@ -212,7 +212,9 @@ class DataForm extends BaseModel{
         }
         /*$data_arr['user_count'] = $user_query->andWhere(['u.mch_id' => 0, 'u.is_delete' => 0, 'u.mall_id' => \Yii::$app->mall->id,])
             ->count();*///用户数
-        $data_arr['user_count'] = StatisticsVirtualConfig::find()->where(['mall_id'=>\Yii::$app->mall->id,'is_delete'=>0])->select([ 'user_sum'])->asArray()->one();
+        $user_sum = StatisticsVirtualConfig::find()->where(['mall_id'=>\Yii::$app->mall->id,'is_delete'=>0])->select(['user_sum'])->asArray()->one();
+        $data_arr['user_count'] = $user_sum['user_sum'];
+        
         //以下随时间查询改变
         $order_query = Order::find()->alias('o')->where(['o.is_recycle' => 0, 'o.is_delete' => 0, 'o.mall_id' => \Yii::$app->mall->id,])
             ->leftJoin(['i' => User::tableName()], 'i.id = o.user_id')

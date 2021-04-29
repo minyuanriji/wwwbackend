1c7afaff5423313951b19522ded9d2c1f9cf6066
mr.lin <746027209@qq.com>
2021-04-29 15:44:42 +0800

    #

--- run.sh
+++ run.sh
@@ -3,17 +3,25 @@
 basepath=$(cd `dirname $0`; pwd)
 chmod a+x "$basepath/yii"
 
-command="/www/server/php/73/bin/php $basepath/yii add-queue-task/execute"
+commands[0]="/www/server/php/73/bin/php $basepath/yii add-queue-task/execute"
+commands[1]="/www/server/php/73/bin/php $basepath/yii user-relationship-link/maintant-job"
+commands[1]="/www/server/php/73/bin/php $basepath/yii commission/maintant-job"
 
-ps -ef | grep "`echo $command`"|awk '{print $2}'|xargs kill -9
 
-result=$(ps -ef | grep "`echo $command`" | grep -v "grep")
+for command in ${commands}
+do
 
-if [ ! -n "$result" ]
-then
-  echo "Starting the process."
-  str=$(nohup $command >/dev/null &)
-  echo -e "\033[32mOk.\033[0m"
-else
-  echo "The process has been started."
-fi
+	ps -ef | grep "`echo $command`"|awk '{print $2}'|xargs kill -9
+
+	result=$(ps -ef | grep "`echo $command`" | grep -v "grep")
+
+	if [ ! -n "$result" ]
+	then
+	  echo "Starting the process."
+	  str=$(nohup $command >/dev/null &)
+	  echo -e "\033[32mOk.\033[0m"
+	else
+	  echo "The process has been started."
+	fi
+
+done

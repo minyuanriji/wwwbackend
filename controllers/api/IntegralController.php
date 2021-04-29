c515e4044263ed014364cea590cea9c6e136788b
wmc <205176371@qq.com>
2021-04-29 14:25:57 +0800

    修改红包页面分页

--- controllers/api/IntegralController.php
+++ controllers/api/IntegralController.php
@@ -54,7 +54,7 @@ class IntegralController extends ApiController
         $params = array(
             'select'=>'id,user_id,money,desc,before_money,type,expire_time,status,created_at',
             'where' => $where,
-            'page' => $this->page,
+            'page' => $reques_data['page'],
             'limit' => $this->pageSize,
             'order' => 'id DESC'
         );

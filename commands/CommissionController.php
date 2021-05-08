7b2f9407b3d41b4237d7b71aed8ec24ec55ac470
wmc <205176371@qq.com>
2021-05-08 09:06:42 +0800

    添加商家推荐人分佣

--- commands/CommissionController.php
+++ commands/CommissionController.php
@@ -10,6 +10,8 @@ use app\models\UserRelationshipLink;
 use app\plugins\commission\models\CommissionCheckoutPriceLog;
 use app\plugins\commission\models\CommissionGoodsPriceLog;
 use app\plugins\commission\models\CommissionRuleChain;
+use app\plugins\commission\models\CommissionRules;
+use app\plugins\commission\models\CommissionStorePriceLog;
 use app\plugins\mch\models\MchCheckoutOrder;
 use yii\db\ActiveQuery;
 
@@ -31,6 +33,9 @@ class CommissionController extends BaseCommandController{
 
                 //店铺二维码收款分佣
                 $this->checkoutOrderNew();
+
+                //店铺二维码收款上级推荐人分佣
+                $this->storeOrderNew();
 
             }catch (\Exception $e){
                 $this->commandOut($e->getMessage());
@@ -137,6 +142,132 @@ class CommissionController extends BaseCommandController{
 
 
                 }
+            }catch (\Exception $e){
+                $this->commandOut($e->getMessage());
+            }
+
+
+
+            MchCheckoutOrder::updateAll([
+                "commission_status" => 1
+            ], ["id" => $checkoutOrder['id']]);
+        }
+
+        return true;
+    }
+
+    /**
+     * 二维码收款订单，新增分佣记录----店铺二维码收款上级推荐人分佣
+     * @return boolean
+     */
+    private function storeOrderNew(){
+        $query = MchCheckoutOrder::find()->alias("mco");
+        $query->innerJoin("{{%store}} s", "s.id=mco.store_id");
+        $query->innerJoin("{{%plugin_mch}} m", "m.id=s.mch_id");
+        $query->andWhere([
+            "AND",
+            ["mco.is_pay" => 1],
+            ["mco.is_delete" => 0],
+            ["mco.commission_status" => 0]
+        ]);
+        $query->select(["mco.*", "s.name", "m.transfer_rate","m.user_id"]);
+        $checkoutOrders = $query->asArray()->limit(10)->all();
+        if(!$checkoutOrders){
+            return false;
+        }
+
+        foreach($checkoutOrders as $checkoutOrder)
+        {
+            try {
+                //获取店铺用户信息及上级用户
+                $user = User::findOne($checkoutOrder['user_id']);
+                if(!$user){
+                    throw new \Exception("商铺用户[ID:".($user ? $user->id : 0)."]不存在");
+                }
+
+                $parent_user = User::findOne($user->parent_id);
+                if(!$parent_user){
+                    throw new \Exception("商铺上级用户[ID:".($parent_user ? $parent_user->id : 0)."]不存在");
+                }
+
+                //获取当前店铺分佣规则
+                $query = CommissionRules::find()->alias("cr");
+                $query->leftJoin("{{%plugin_commission_rule_chain}} crc", "cr.id=crc.rule_id");
+                $query->andWhere([
+                    "AND",
+                    ["cr.item_type"  => 'store'],
+                    ["cr.item_id"    => $checkoutOrder['store_id']],
+                    ['cr.is_delete'  => 0],
+                ]);
+                $commission_res = $query->select(["cr.commission_type", "crc.level", "crc.commisson_value"])->asArray()->one();
+
+                if ($commission_res) {
+                    //计算分佣金额
+                    $transferRate = (int)$checkoutOrder['transfer_rate'];//商户手续费
+                    $commission_res['profit_price'] = ($transferRate/100) * $checkoutOrder['order_price'];
+                    if($commission_res['commission_type'] == 1){ //按百分比
+                        $price = (floatval($commission_res['commisson_value'])/100) * floatval($commission_res['profit_price']);
+                    }else{ //按固定值
+                        $price = (float)$commission_res['commisson_value'];
+                    }
+
+                    //生成分佣记录
+                    if($price > 0){
+                        $priceLog = CommissionCheckoutPriceLog::findOne([
+                            "checkout_order_id" => $checkoutOrder['id'],
+                            "user_id"           => $user->parent_id,
+                        ]);
+                        if(!$priceLog){ //没有生成过再去生成
+                            $trans = \Yii::$app->db->beginTransaction();
+                            try {
+                                $priceLog = new CommissionStorePriceLog([
+                                    "mall_id"           => $checkoutOrder['mall_id'],
+                                    "item_id"           => $checkoutOrder['id'],
+                                    "item_type"         => 'checkout',
+                                    "user_id"           => $user->parent_id,
+                                    "price"             => round($price, 5),
+                                    "status"            => 1,
+                                    "created_at"        => $checkoutOrder['created_at'],
+                                    "updated_at"        => $checkoutOrder['updated_at'],
+                                    "rule_data_json"    => json_encode($commission_res)
+                                ]);
+                                if(!$priceLog->save()){
+                                    throw new \Exception(json_encode($priceLog->getErrors()));
+                                }
+                                $this->commandOut("生成分佣记录 [ID:".$priceLog->id."]");
+
+                                //收入记录
+                                $incomeLog = new IncomeLog([
+                                    'mall_id'     => $checkoutOrder['mall_id'],
+                                    'user_id'     => $user->parent_id,
+                                    'type'        => 1,
+                                    'money'       => $parent_user['total_income'],
+                                    'income'      => $priceLog->price,
+                                    'desc'        => "来自店铺“".$checkoutOrder['name']."”的分佣记录[ID:".$priceLog->id."]",
+                                    'flag'        => 1, //到账
+                                    'source_id'   => $priceLog->id,
+                                    'source_type' => 'checkout',
+                                    'created_at'  => $checkoutOrder['created_at'],
+                                    'updated_at'  => $checkoutOrder['updated_at']
+                                ]);
+                                if(!$incomeLog->save()){
+                                    throw new \Exception(json_encode($incomeLog->getErrors()));
+                                }
+
+                                User::updateAllCounters([
+                                    "total_income"  => $priceLog->price,
+                                    "income" => $priceLog->price
+                                ], ["id" => $parent_user['id']]);
+
+                                $trans->commit();
+                            }catch (\Exception $e){
+                                $trans->rollBack();
+                                $this->commandOut($e->getMessage());
+                            }
+                        }
+                    }
+                }
+
             }catch (\Exception $e){
                 $this->commandOut($e->getMessage());
             }

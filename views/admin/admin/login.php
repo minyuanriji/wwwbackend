dde5e0079374a9dd4e71d892e419c625b888f93e
wmc <205176371@qq.com>
2021-05-13 17:42:37 +0800

    修改后台logo样式

--- views/admin/admin/login.php
+++ views/admin/admin/login.php
@@ -88,9 +88,8 @@ $indSetting = \app\logic\OptionLogic::get(\app\models\Option::NAME_IND_SETTING);
         left: 0;
         bottom: 0;
         margin: auto;
-        padding: 150px 60px 180px;
-        width: 470px;
-        min-height: 100%;
+        width: 370px;
+        height: 400px;
         background-color: #fff;
     }
 
@@ -127,8 +126,8 @@ $indSetting = \app\logic\OptionLogic::get(\app\models\Option::NAME_IND_SETTING);
             <div class="site-content">
                 <div class="brand-info">
 <!--                    <img class="logo" :src="login_logo" alt="">-->
-                    <h2 class="brand-info__text">补商汇后台管理系统</h2>
-                    <p class="brand-info__intro">补商汇商城后台管理系统。</p>
+                    <!--<h2 class="brand-info__text">补商汇后台管理系统</h2>
+                    <p class="brand-info__intro">补商汇商城后台管理系统。</p>-->
                 </div>
                 <div class="login-main">
                     <h3 class="login-title">{{user_type == 1 ? '管理员' : '员工'}}登录</h3>

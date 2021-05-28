dd0cca6e45c8d4a803e882d934af238ea0b5b2d1
mr.lin <746027209@qq.com>
2021-05-28 11:25:54 +0800

    #11:25

--- commands/WsClientController.php
+++ commands/WsClientController.php
@@ -8,9 +8,10 @@ class WsClientController extends BaseCommandController {
         \Swoole\Coroutine\run(function () {
             $cli = new \Swoole\Coroutine\Http\Client('81.71.7.222', 9515);
             $data = "PAID:" . json_encode(["text" => "11111", "url" => "https://img-qn.51miz.com/preview/sound/00/27/20/51miz-S272046-BC428C3F.mp3"]);
-            $cli->get('/?MCH_PAID_NOTIFY_MOBILE=13422078495&MCH_PAID_NOTIFY_DATA=' . $data);
+            $cli->get('/?action=MchPaidNotify&notify_mobile=13422078495&notify_data=' . $data);
             $cli->close();
         });
     }
+
 
 }
--- mch/forms/api/BindDeviceForm.php
+++ mch/forms/api/BindDeviceForm.php
@@ -7,11 +7,31 @@ use app\models\BaseModel;
 
 class BindDeviceForm extends BaseModel{
 
-    public function bind(){
+    public $mobile;
+    public $verify_code;
+
+    public function rules(){
         return [
-            'code' => ApiCode::CODE_SUCCESS,
-            'msg'  => '操作成功'
+            [['mobile', 'verify_code'], 'required']
         ];
+    }
+
+    public function bind(){
+
+        if(!$this->validate()){
+            return $this->responseErrorInfo();
+        }
+
+        try {
+            throw new \Exception("商户不存在");
+        }catch (\Exception $e){
+            return [
+                'code' => ApiCode::CODE_FAIL,
+                'msg'  => $e->getMessage()
+            ];
+        }
+
+
     }
 
 }

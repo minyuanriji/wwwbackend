3feb8ee49d5d1d1382cc0e200efcc7f324cdc5b5
zal <59867179@qq.com>
2021-06-12 11:48:03 +0800

    merge

--- controllers/JobDebugController.php
+++ controllers/JobDebugController.php
@@ -1,12 +1,19 @@
 <?php
 namespace app\controllers;
 
+use app\models\Mall;
 use yii\web\Controller;
 
-class JobDebugController extends Controller{
+class JobDebugController extends BaseController {
 
     public function actionIndex(){
-
+        $this->setWechatParmas(5);
+        $wechatModel = \Yii::$app->wechat;
+        if(true || $wechatModel->isWechat){
+            $info = $wechatModel->app->user->get("oHQr7wg8Hf45__91vpq5VksCSK_U");
+            print_r($info);
+            exit;
+        }
 
     }
 

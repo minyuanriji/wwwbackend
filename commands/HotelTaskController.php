2e583c1ce0eb62750f685b3a1f0d3fff910a7648
mr.lin <746027209@qq.com>
2021-06-29 09:21:43 +0800

    9:21

--- commands/HotelTaskController.php
+++ commands/HotelTaskController.php
@@ -3,51 +3,36 @@ namespace app\commands;
 
 
 
+use yii\base\InvalidRouteException;
 
 class HotelTaskController extends BaseCommandController {
 
     public $tasks = [
-        ["num" => 1,  "name" => "Query",  "workerIds" => []],
-        ["num" => 50, "name" => "Search", "workerIds" => []]
+        ["num" => 1,  "name" => "Query", "action" => "query"],
+        ["num" => 50, "name" => "Search", "action" => "search"]
     ];
 
-    public function start(){
-        $num = 0;
-        foreach($this->tasks as $task){
-            $num += $task['num'];
-        }
-        $pool = new \Swoole\Process\Pool($num);
-        $pool->set(['enable_coroutine' => true]);
-        $pool->on('WorkerStart', function (\Swoole\Process\Pool $pool, $workerId){
-            echo("[Worker #{$workerId}] WorkerStart, pid: " . posix_getpid() . "\n");
-            foreach($this->tasks as $key => $task){
-                if(count($task['workerIds']) < $task['num']){
-                    $this->tasks[$key]['workerIds'][$workerId] = 1;
-                    break;
-                }
-            }
-            foreach($this->tasks as $task){
-                $workerIds = array_keys($task['workerIds']);
-                if(in_array($workerId, $workerIds)){
-                    while(true){
-                        echo $task['name'] . "\n";
-                        sleep(1);
-                    }
-                }
-            }
-        });
-        $pool->on('WorkerStop', function (\Swoole\Process\Pool $pool, $workerId) {
-            echo("[Worker #{$workerId}] WorkerStop\n");
-            foreach($this->tasks as $key => $task){
-                $workerIds = array_keys($task['workerIds']);
-                if(in_array($workerId, $workerIds)){
-                    "Remove From " . $task['name'] . "\n";
-                    unset($this->tasks[$key]['workerIds'][$workerId]);
-                    break;
-                }
-            }
-        });
-        $pool->start();
+    public function actions(){
+        return [
+            'query' => 'app\hotel_task_action\QueryAction'
+        ];
     }
 
+    public function actionStart(){
+        $pm = new \Swoole\Process\ProcessManager();
+        foreach($this->tasks as $task){
+            for($i=0; $i < $task['num']; $i++){
+                $pm->add(function (\Swoole\Process\Pool $pool, int $workerId) use($task) {
+                    try {
+                        $this->runAction($task['action']);
+                        $this->commandOut($task['name'] . " task start successfully,worker:{$workerId}");
+                    }catch (InvalidRouteException $e){
+                        $this->commandOut($e->getMessage());
+                        while(true){}
+                    }
+                });
+            }
+        }
+        $pm->start();
+    }
 }
---
+++ commands/hotel_task_action/QueryAction.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\commands\hotel_task_action;
+
+use yii\base\Action;
+
+class QueryAction extends Action{
+
+    public function run(){
+        while(true){
+
+        }
+    }
+}

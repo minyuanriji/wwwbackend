d82466138bbc9897f50147bc958bd434edd94d4d
mr.lin <746027209@qq.com>
2021-06-30 11:38:18 +0800

    11:38

--- commands/HotelImportController.php
+++ commands/HotelImportController.php
@@ -1,39 +1,49 @@
 <?php
 namespace app\commands;
 
-use app\core\ApiCode;
 use app\models\Mall;
-use app\plugins\hotel\forms\mall\HotelImportForm;
 
-class HotelImportController extends BaseCommandController
-{
+class HotelImportController extends BaseCommandController{
+
     /**
      * 酒店数据同步
      */
-    public function actionUpdate(){
+    public function actionStart(){
         $this->mutiKill(); //只能只有一个维护服务
 
-        \Yii::$app->mall = Mall::findOne(5);
+        $demoData = [
+            "page_num" => 5000,
+            "page_size" => 5,
+            "plateform_class" => "app\\plugins\\hotel\\libs\\bestwehotel\\PlateForm"
+        ];
 
-        $page = 1;
-        while (true){
-            $form = new HotelImportForm();
-            $form->page = $page;
-            $form->size = 5;
-            $form->plateform_class = "app\\plugins\\hotel\\libs\\bestwehotel\\PlateForm";
-            $res = $form->import();
-            if($res['code'] != ApiCode::CODE_SUCCESS){
-                $this->commandOut("error:{$page} for " . $res['msg']);
-            }else{
-                if($res['data']['next_page'] <= 0){
-                    $this->commandOut("finished!");
-                    exit;
-                }
-                $prograss = round(100 * (($page)/$res['data']['total_pages']), 2);
-                $this->commandOut("Prograss:{$prograss}%");
-            }
-            $page += 1;
+        $tasks = [];
+        for($i=1; $i <= $demoData['page_num']; $i++){
+            $tasks[] = [
+                'page' => $i,
+                'size' => $demoData['page_size'],
+                'plateform_class' => $demoData['plateform_class']
+            ];
         }
 
+        $pm = new \Swoole\Process\ProcessManager();
+        for($i=0; $i <= 50; $i++){
+            $pm->add(function (\Swoole\Process\Pool $pool, int $workerId) use($tasks){
+                $this->commandOut("[Worker #{$workerId}] WorkerStart, pid: " . posix_getpid());
+                if(!defined("Yii")){
+                    require_once(__DIR__ . '/../vendor/autoload.php');
+                    require_once __DIR__ . '/../config/const.php';
+                    new \app\core\ConsoleApplication();
+                }
+                \Yii::$app->mall = Mall::findOne(5);
+                while(true){
+                    if(!empty($tasks)){
+                        $task = array_shift($tasks);
+                        $this->commandOut("task:" . $task['page']);
+                    }
+                }
+            });
+        }
+        $pm->start();
     }
 }

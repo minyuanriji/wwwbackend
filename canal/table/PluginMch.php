5f84071819a097f8f1ffaa3c50a6b111850baabe
wmc <205176371@qq.com>
2021-07-01 14:44:14 +0800

    门店审核通知

--- canal/table/PluginMch.php
+++ canal/table/PluginMch.php
@@ -1,27 +1,28 @@
 <?php
+
 namespace app\canal\table;
 
 use app\notification\MchApplyPassedNotification;
 use app\plugins\mch\models\Mch;
 use Yii;
 
-class PluginMch{
+class PluginMch
+{
 
-    public function insert($rows){
+    public function insert($rows)
+    {
 
     }
 
-    public function update($mixDatas){
-
-        foreach($mixDatas as $mixData){
+    public function update($mixDatas)
+    {
+        foreach ($mixDatas as $mixData) {
             $condition = $mixData['condition'];
             $update = $mixData['update'];
-            if(isset($update['review_status'])){
-                if($update['review_status'] == Mch::REVIEW_STATUS_CHECKED){
-
+            if (isset($update['review_status'])) {
+                if ($update['review_status'] > Mch::REVIEW_STATUS_UNCHECKED) {
                     $mch = Mch::find()->where($condition)->one();
                     $mch && MchApplyPassedNotification::send($mch);
-
                 }
             }
         }

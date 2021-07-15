9fb4c2ce442433031449141f334e7b1266bdf354
mr.lin <746027209@qq.com>
2021-07-15 16:39:23 +0800

    16:39

--- mch/forms/api/BindDeviceForm.php
+++ mch/forms/api/BindDeviceForm.php
@@ -9,15 +9,19 @@ use app\models\Admin;
 use app\models\BaseModel;
 use app\models\User;
 use app\plugins\mch\models\Mch;
+use app\plugins\mch\models\MchSubAccount;
 
 class BindDeviceForm extends BaseModel{
 
     public $mobile;
     public $verify_code;
+    public $sub_login = 0;
+    public $main_mobile;
 
     public function rules(){
         return [
-            [['mobile', 'verify_code'], 'required']
+            [['mobile', 'verify_code'], 'required'],
+            [['sub_login', 'main_mobile'], 'safe']
         ];
     }
 
@@ -29,58 +33,89 @@ class BindDeviceForm extends BaseModel{
 
         try {
             //手机验证码验证
-            /*$smsForm = new SmsForm();
+            $smsForm = new SmsForm();
             $smsForm->mobile = $this->mobile;
             $smsForm->captcha = $this->verify_code;
             if (!$smsForm->checkCode()) {
                 throw new \Exception("验证码不正确");
-            }*/
+            }
 
-            $mch = Mch::findOne([
-                'mobile'    => $this->mobile,
-                'is_delete' => 0
-            ]);
+            if($this->sub_login){ //子账号登陆
+                if(empty($this->main_mobile)){
+                    throw new \Exception("请填写主账号绑定的手机");
+                }
+                $mch = Mch::findOne([
+                    'mobile'    => $this->main_mobile,
+                    'is_delete' => 0
+                ]);
+            }else{
+                $mch = Mch::findOne([
+                    'mobile'    => $this->mobile,
+                    'is_delete' => 0
+                ]);
+            }
+
             if(!$mch){
-                throw new \Exception("无法获取到商户信息");
+                throw new \Exception("商户不存在");
+            }
+
+            if($mch->review_status != Mch::REVIEW_STATUS_CHECKED){
+                throw new \Exception("该商户仍在审核中");
             }
 
             $security = \Yii::$app->getSecurity();
 
-            $admin = Admin::findOne(["mch_id" => $mch->id]);
-            if(!$admin){
-                $admin = new Admin([
-                    'username'     => "A:" . uniqid(),
-                    'password'     => $security->generatePasswordHash(uniqid()),
-                    'mall_id'      => 5,
-                    'mch_id'       => $mch->id,
-                    'admin_type'   => '3',
-                    'mall_num'     => 0,
-                    'expired_at'   => 0,
-                    'is_delete'    => 0,
-                    'created_at'   => time(),
-                    'updated_at'   => time()
-                ]);
+            $subUser = null;
+            if($this->sub_login){ //子账号登陆
+                $subUser = User::find()->alias("u")->select(["u.*"])
+                            ->innerJoin(["msa" => MchSubAccount::tableName()], "msa.user_id=u.id")
+                            ->where(["u.mobile" => $this->mobile])->one();
+                if(!$subUser || $subUser->is_delete){
+                    throw new \Exception("子账号不存在");
+                }
+
+                $subUser->auth_key = \Yii::$app->security->generateRandomString();
+                $subUser->access_token = \Yii::$app->security->generateRandomString();
+                if(!$subUser->save()){
+                    throw new \Exception(json_encode($subUser->getErrors()));
+                }
+            }else{
+                $admin = Admin::findOne(["mch_id" => $mch->id]);
+                if(!$admin){
+                    $admin = new Admin([
+                        'username'     => "A:" . uniqid(),
+                        'password'     => $security->generatePasswordHash(uniqid()),
+                        'mall_id'      => 5,
+                        'mch_id'       => $mch->id,
+                        'admin_type'   => '3',
+                        'mall_num'     => 0,
+                        'expired_at'   => 0,
+                        'is_delete'    => 0,
+                        'created_at'   => time(),
+                        'updated_at'   => time()
+                    ]);
+                }
+
+                $admin->auth_key = \Yii::$app->security->generateRandomString();
+                $admin->access_token = \Yii::$app->security->generateRandomString();
+
+                if(!$admin->save()){
+                    throw new \Exception(json_encode($admin->getErrors()));
+                }
+
+                $user = User::findOne($mch->user_id);
+                if(!$user || $user->is_delete){
+                    throw new \Exception("无法获取到商户所属小程序账号");
+                }
+
+                $user->auth_key = \Yii::$app->security->generateRandomString();
+                $user->access_token = \Yii::$app->security->generateRandomString();
+                if(!$user->save()){
+                    throw new \Exception(json_encode($user->getErrors()));
+                }
             }
 
-            $admin->auth_key = \Yii::$app->security->generateRandomString();
-            $admin->access_token = \Yii::$app->security->generateRandomString();
-
-            if(!$admin->save()){
-                throw new \Exception("账号保存失败");
-            }
-
-            $user = User::findOne($mch->user_id);
-            if(!$user || $user->is_delete){
-                throw new \Exception("无法获取到商户所属小程序账号");
-            }
-
-            $user->auth_key = \Yii::$app->security->generateRandomString();
-            $user->access_token = \Yii::$app->security->generateRandomString();
-            if(!$user->save()){
-                throw new \Exception("小程序账号信息保存失败");
-            }
-
-            //Sms::updateCodeStatus($this->mobile, $this->verify_code);
+            Sms::updateCodeStatus($this->mobile, $this->verify_code);
 
             return [
                 'code' => ApiCode::CODE_SUCCESS,
@@ -88,7 +123,8 @@ class BindDeviceForm extends BaseModel{
                 'data' => [
                     'auth_key'     => $user->auth_key,
                     'mobile'       => $this->mobile,
-                    'access_token' => $user->access_token
+                    'mch_id'       => $mch->id,
+                    'access_token' => $subUser ? $subUser->access_token : $user->access_token
                 ]
             ];
 

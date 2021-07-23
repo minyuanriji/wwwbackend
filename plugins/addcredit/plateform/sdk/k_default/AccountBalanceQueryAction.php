d250e2f72ba1ea63471a318a5e23488d79175706
wmc <205176371@qq.com>
2021-07-23 11:58:46 +0800

    测试

--- plugins/addcredit/plateform/sdk/k_default/AccountBalanceQueryAction.php
+++ plugins/addcredit/plateform/sdk/k_default/AccountBalanceQueryAction.php
@@ -2,6 +2,7 @@
 
 namespace app\plugins\addcredit\plateform\sdk\k_default;
 
+use app\controllers\business\WechatTemplate;
 use app\core\ApiCode;
 use app\plugins\addcredit\forms\common\Request;
 use app\plugins\addcredit\plateform\result\BalanceQueryResult;
@@ -16,13 +17,21 @@ class AccountBalanceQueryAction extends BaseObject
     {
         $BalanceQueryResult = new BalanceQueryResult();
         try {
-            $post_param = [
+            /*$post_param = [
                 'szAgentId'      => $this->plateforms_params['id'],
                 'szVerifyString' => md5("szAgentId=" . $this->plateforms_params['id'] . "&szKey=" . $this->plateforms_params['secret_key']),
                 'szFormat'       => "JSON"
+            ];*/
+            $post_param = [
+                'szAgentId'      => 200025,
+                'szVerifyString' => md5("szAgentId=200025&szKey=6a299acc10ef41a2b59d031a49ccac68"),
+                'szFormat'       => "JSON"
             ];
+            /*$wech =  new WechatTemplate();
+            $res = $wech->http_get("http://120.25.166.45:10186/plat/api/old/queryBalance?szAgentId=200025&szVerifyString=418649684a0489aa27476ae1cddd84c9&szFormat=JSON");*/
             $response = Request::execute(Config::BALANCE_QUERY, $post_param);
             print_r($response);die;
+
             $parseArray = @json_decode($response, true);
             if (!isset($parseArray['nRtn'])) {
                 throw new \Exception("解析数据错误", ApiCode::CODE_FAIL);

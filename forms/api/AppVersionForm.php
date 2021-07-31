ea8596dc145fa860f6d9ab3cba4f69be03253424
mr.lin <746027209@qq.com>
2021-07-31 14:53:20 +0800

    获取app版本接口

---
+++ forms/api/AppVersionForm.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\forms\api;
+
+
+use app\core\ApiCode;
+use app\models\Apps;
+use app\models\BaseModel;
+
+class AppVersionForm extends BaseModel{
+
+    public $platform;
+
+    public function rules(){
+        return [
+            [['platform'], 'required']
+        ];
+    }
+
+    public function getVersion(){
+
+        if(!$this->validate()){
+            return $this->responseErrorInfo();
+        }
+
+        try {
+
+            $version = Apps::find()->where([
+                "type"      => "merchant",
+                "platform"  => "android",
+                "is_delete" => 0
+            ])->asArray()->orderBy("version_code DESC")->one();
+
+            if(!$version){
+                throw new \Exception("无法获取版本信息");
+            }
+
+            return [
+                'code' => ApiCode::CODE_FAIL,
+                'data' => [
+                    'version_code'  => $version['version_code'],
+                    'version_name'  => $version['version_name'],
+                    'download_link' => $version['download_link'],
+                    'date'          => date("Y-m-d", $version['updated_at'])
+                ]
+            ];
+        }catch (\Exception $e){
+            return [
+                'code' => ApiCode::CODE_FAIL,
+                'msg'  => $e->getMessage()
+            ];
+        }
+
+    }
+
+}

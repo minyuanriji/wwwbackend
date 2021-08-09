05aa020e628e7cd5c7d9b6b379f53904c781725e
mr.lin <746027209@qq.com>
2021-08-09 15:34:09 +0800

    拼单详情返回大礼包详情

--- plugins/giftpacks/forms/api/GiftpacksGroupDetailForm.php
+++ plugins/giftpacks/forms/api/GiftpacksGroupDetailForm.php
@@ -6,6 +6,7 @@ namespace app\plugins\giftpacks\forms\api;
 use app\core\ApiCode;
 use app\models\BaseModel;
 use app\models\User;
+use app\plugins\giftpacks\models\Giftpacks;
 use app\plugins\giftpacks\models\GiftpacksGroup;
 use app\plugins\giftpacks\models\GiftpacksGroupPayOrder;
 
@@ -31,7 +32,7 @@ class GiftpacksGroupDetailForm extends BaseModel{
                         ->innerJoin(["u" => User::tableName()], "u.id=gg.user_id")
                         ->where(["gg.id" => $this->group_id])
                         ->select([
-                            "gg.id", "gg.user_id", "gg.need_num", "gg.user_num",
+                            "gg.id", "gg.pack_id", "gg.user_id", "gg.need_num", "gg.user_num",
                             "gg.status", "gg.expired_at", "u.nickname", "u.avatar_url"
                         ])->asArray()->one();
             if(!$groupInfo){
@@ -41,6 +42,12 @@ class GiftpacksGroupDetailForm extends BaseModel{
             $groupInfo['still_need_num'] = intval($groupInfo['need_num']) - intval($groupInfo['user_num']);
             unset($groupInfo['need_num']);
             unset($groupInfo['user_num']);
+
+            //获取大礼包
+            $giftpacks = Giftpacks::findOne($groupInfo['pack_id']);
+            if(!$giftpacks || $giftpacks->is_delete){
+                throw new \Exception("大礼包不存在");
+            }
 
             //获取参与记录
             $joinList = GiftpacksGroupPayOrder::find()->alias("ggpo")
@@ -58,8 +65,9 @@ class GiftpacksGroupDetailForm extends BaseModel{
             return [
                 'code' => ApiCode::CODE_SUCCESS,
                 'data' => [
-                    "group_info" => $groupInfo,
-                    "join_list"  => $joinList
+                    "group_info"     => $groupInfo,
+                    "join_list"      => $joinList,
+                    "giftpacks_info" => GiftpacksDetailForm::detail($giftpacks)
                 ]
             ];
         }catch (\Exception $e){

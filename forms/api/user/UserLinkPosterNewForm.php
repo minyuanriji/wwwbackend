92fa1a25eeaa1fd932cd7ad9f6d28fa0657021c4
林工 <746027209@qq.com>
2021-08-11 19:10:36 +0800

    19：10

--- forms/api/user/UserLinkPosterNewForm.php
+++ forms/api/user/UserLinkPosterNewForm.php
@@ -36,22 +36,54 @@ class UserLinkPosterNewForm extends GrafikaOption implements BasePoster{
                 "size" => 240,
                 "top" => 1030,
                 "left" => 460,
-                "type" => 1,
+                "type" => 0,
                 "file_type" => "image"
             ];
-
 
             if(\Yii::$app->appPlatform == User::PLATFORM_MP_WX){
                 $file = $this->qrcode($option, [
                     [
-                        'pid'     => \Yii::$app->user->id
+                        'pid' => \Yii::$app->user->id
                     ],
                     280,
                     'pages/index/index'
                 ], $this);
-                print_r($file);
-                exit;
             }
+
+            $option = array_merge($option, [
+                'bg_url' => \Yii::$app->basePath . '/web/statics/bg/redpack.png',//背景图片路径
+                'text' => [
+                    [
+                        'text' => '',//扫码领红包
+                        'left' => 116,
+                        'top' => 280,
+                        'width' => 300,
+                        'fontSize' => 14, //字号
+                        'fontColor' => '255,255,255', //字体颜色
+                        'angle' => 0,
+                    ]
+                ],
+                'image' => [
+                    [
+                        'name' => '二维码', //图片名称，用于出错时定位
+                        'url' => '',
+                        'stream' => file_get_contents($file),
+                        'left' => 190,
+                        'top' => 390,
+                        'right' => 0,
+                        'bottom' => 0,
+                        'width' => 380,
+                        'height' => 380,
+                        'radius' => 0,
+                        'opacity' => 100
+                    ]
+                ]
+            ]);
+
+            $editor = $this->getPoster($option);
+
+            echo $editor->qrcode_url . '?v=' . time();
+            exit;
 
         }catch (\Exception $e){
             return [

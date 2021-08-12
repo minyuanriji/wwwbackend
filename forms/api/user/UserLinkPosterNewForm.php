12edfb26272c90e79dde6fc67fefe05fb7bcead3
林工 <746027209@qq.com>
2021-08-12 15:33:16 +0800

    15：33

--- forms/api/user/UserLinkPosterNewForm.php
+++ forms/api/user/UserLinkPosterNewForm.php
@@ -3,18 +3,16 @@
 
 namespace app\forms\api\user;
 
+use app\controllers\business\Poster;
 use app\core\ApiCode;
 use app\forms\api\poster\BasePoster;
-use app\forms\common\grafika\CustomizeFunction;
 use app\forms\common\grafika\GrafikaOption;
-use app\logic\AppConfigLogic;
+use app\forms\common\QrCodeCommon;
 use app\models\User;
 
 class UserLinkPosterNewForm extends GrafikaOption implements BasePoster{
 
     public $flag;
-
-    use CustomizeFunction;
 
     public function rules(){
         return [
@@ -30,24 +28,54 @@ class UserLinkPosterNewForm extends GrafikaOption implements BasePoster{
 
         try {
 
-            $shareUrl = \Yii::$app->request->hostInfo . '/h5/#/pages/index/index?user_id='. \Yii::$app->user->id . '&pid=' . \Yii::$app->user->id;
-
-            $default = (new \app\forms\mall\poster\PosterForm())->getDefault()['share'];
-            $options = AppConfigLogic::getPosterConfig();
-            $option = $options["share"];
-            $option = $this->optionDiff($option, $default);
-
-            $option['bg_pic']['url'] = \Yii::$app->basePath . "/web/statics/bg/redpack.png";
-
             if(\Yii::$app->appPlatform == User::PLATFORM_MP_WX){
-                $file = $this->qrcode($option, [['pid' => \Yii::$app->user->id], 280, 'pages/index/index' ], $this);
+                $code = (new QrCodeCommon())->getQrCode(['pid' => \Yii::$app->user->id], 350, 'pages/index/index');
             }
 
-            $editor = $this->getPoster($option);
+            $config = array(
+                'bg_url' => \Yii::$app->basePath . '/web/statics/bg/redpack.png',//背景图片路径
+                'text' => [
+                    [
+                        'text' => '',//扫码领红包
+                        'left' => 116,
+                        'top' => 280,
+                        'width' => 300,
+                        'fontSize' => 14, //字号
+                        'fontColor' => '255,255,255', //字体颜色
+                        'angle' => 0,
+                    ]
+                ],
+                'image' => [
+                    [
+                        'name' => '二维码', //图片名称，用于出错时定位
+                        'url' => '',
+                        'stream' => file_get_contents($code['file_path']),
+                        'left' => 190,
+                        'top' => 390,
+                        'right' => 0,
+                        'bottom' => 0,
+                        'width' => 380,
+                        'height' => 380,
+                        'radius' => 0,
+                        'opacity' => 100
+                    ]
+                ]
+            );
+            Poster::setConfig($config);
 
-            echo $editor->qrcode_url . '?v=' . time();
-            exit;
+            //设置保存路径
+            $fileName =  "/web/temp/" . md5(uniqid()) . ".jpg";
+            if(!Poster::make(\Yii::$app->basePath . $fileName)){
+                throw new \Exception("分享海报生成失败");
+            }
 
+            //是否要清理缓存资源
+            Poster::clear();
+
+            return [
+                'status' => 1,
+                'img'    => \Yii::$app->request->hostInfo . $fileName
+            ];
         }catch (\Exception $e){
             return [
                 'code' => ApiCode::CODE_FAIL,

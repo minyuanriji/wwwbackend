c853e92f4c078b6a56137107852c488931699b33
mr.lin <746027209@qq.com>
2021-09-20 13:50:00 +0800

    13:49

--- plugins/alibaba/forms/mall/AlibabaDistributionAliGoodsImportForm.php
+++ plugins/alibaba/forms/mall/AlibabaDistributionAliGoodsImportForm.php
@@ -58,14 +58,16 @@ class AlibabaDistributionAliGoodsImportForm extends BaseModel{
                     ]));
                 }
 
-                $goods->name            = $info['title'];
-                $goods->cover_url       = $info['imgUrl'];
-                $goods->updated_at      = time();
-                $goods->ali_data_json   = json_encode($info);
-                $goods->price           = $info['currentPrice'];
-                $goods->origin_price    = $info['currentPrice'];
-                $goods->ali_category_id = 0;
-                $goods->is_delete       = 0;
+                $goods->name              = $info['title'];
+                $goods->cover_url         = $info['imgUrl'];
+                $goods->updated_at        = time();
+                $goods->ali_data_json     = json_encode($info);
+                $goods->price             = $info['currentPrice'];
+                $goods->price_rate        = 100;
+                $goods->origin_price_rate = 100;
+                $goods->origin_price      = $info['currentPrice'];
+                $goods->ali_category_id   = 0;
+                $goods->is_delete         = 0;
                 $goods->ali_product_info = json_encode([
                     "biz" => $res->bizGroupInfos,
                     "info" => $res->productInfo

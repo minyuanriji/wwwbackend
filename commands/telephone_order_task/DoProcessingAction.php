5d397a2d75ac5edb445142886f2913f8f1c0b17b
林工 <746027209@qq.com>
2021-10-09 14:56:53 +0800

    14.56

--- commands/telephone_order_task/DoProcessingAction.php
+++ commands/telephone_order_task/DoProcessingAction.php
@@ -104,11 +104,11 @@ class DoProcessingAction extends Action{
             $model->save();
 
             //如果失败次数超过3次，就不再处理
-            //if($order && $order->request_num > 3){
+            /*if($order && $order->request_num > 3){
                 $order->order_status = "fail";
                 $order->updated_at = time();
-            //}
-            $order->save();
+            }
+            $order->save();*/
 
             $this->controller->commandOut($errContent);
         }

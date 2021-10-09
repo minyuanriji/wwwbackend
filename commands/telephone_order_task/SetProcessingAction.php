f55f50b9d8bcafbe9941e42be108a247a2348a71
林工 <746027209@qq.com>
2021-10-09 13:42:41 +0800

    13.42

--- commands/telephone_order_task/SetProcessingAction.php
+++ commands/telephone_order_task/SetProcessingAction.php
@@ -2,13 +2,16 @@
 
 namespace app\commands\telephone_order_task;
 
+use app\core\ApiCode;
 use app\plugins\addcredit\models\AddcreditOrder;
 use app\plugins\addcredit\models\AddcreditOrderThirdParty;
+use app\plugins\addcredit\models\AddcreditPlateforms;
+use app\plugins\addcredit\plateform\sdk\kcb_sdk\PlateForm as kcb_PlateForm;
 use yii\base\Action;
 
 class SetProcessingAction extends Action{
 
-    const VER_START_TIME = "2021-10-09 10:20:00";
+    const VER_START_TIME = "2021-10-09 13:42:00";
 
     public function run(){
         $this->controller->commandOut(date("Y/m/d H:i:s") . " SetProcessingAction start");
@@ -22,15 +25,30 @@ class SetProcessingAction extends Action{
                 $query->andWhere("otp.id IS NULL");
                 $query->andWhere("o.created_at > '".strtotime(self::VER_START_TIME)."'");
 
-                $query->select(["o.id", "o.mall_id"]);
+                $query->select(["o.*"]);
                 $rows = $query->asArray()->limit(10)->all();
                 if ($rows) {
                     foreach ($rows as $row) {
+
+                        $addcreditOrder = new AddcreditOrder();
+                        $addcreditOrder->load(["AddcreditOrder" => $row]);
+
+                        //平台下单
+                        $plateform = AddcreditPlateforms::findOne($row['plateform_id']);
+                        if (!$plateform) {
+                            throw new \Exception("无法获取平台信息");
+                        }
+
+                        $addcreditOrder->order_no = substr(md5(uniqid()), -4) . date("ymdhis") . rand(100000, 999999);
+
+                        $platForm = new kcb_PlateForm();
+                        $res = $platForm->submit($addcreditOrder, $plateform, false);
+
                         $model = new AddcreditOrderThirdParty([
                             "mall_id"         => $row['mall_id'],
                             "order_id"        => $row['id'],
                             "process_status"  => "processing",
-                            "unique_order_no" => substr(md5(uniqid()), -4) . date("ymdhis") . rand(100000, 999999),
+                            "unique_order_no" => $addcreditOrder->order_no,
                             "created_at"      => time()
                         ]);
                         if (!$model->save()) {

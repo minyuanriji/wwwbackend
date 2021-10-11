3f66c77d7dbcf94f1ead782629a1ec93fa90f59d
wmc <205176371@qq.com>
2021-10-11 18:11:43 +0800

    添加酒店海报

--- plugins/hotel/forms/api/HotelInfoForm.php
+++ plugins/hotel/forms/api/HotelInfoForm.php
@@ -30,18 +30,18 @@ class HotelInfoForm extends BaseModel
             //获取首页轮播图
             $resultData['banner'] = [
                 [
-                    'image' => 'https://www.mingyuanriji.cn/web//uploads/images/original/20210922/679cb9aa849b5828c15e22fabb67726c.jpg',
+                    'image' => 'https://www.mingyuanriji.cn/web/static/hotel/hotel-poster1.jpg',
                     'link' => '',
                 ],
                 [
-                    'image' => 'https://www.mingyuanriji.cn/web//uploads/images/original/20210508/dacd08c809c1314a6c4071d45ca7b567.jpg',
+                    'image' => 'https://www.mingyuanriji.cn/web/static/hotel/hotel-poster2.jpg',
                     'link' => '',
                 ],
             ];
 
             //获取首页广告
             $resultData['advert'] = [
-                'image' => 'https://www.mingyuanriji.cn/web//uploads/images/original/20210818/db01559d840db23c955f9ce686ad1384.jpg',
+                'image' => 'https://www.mingyuanriji.cn/web/static/hotel/hotel-advert.jpg',
                 'link' => '',
             ];
 

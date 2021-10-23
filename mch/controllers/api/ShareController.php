abb8235738629fbb1ba7c6ae3cf453920fe768c5
wmc <205176371@qq.com>
2021-10-23 16:32:49 +0800

    修改商户分享海报不显示

--- mch/controllers/api/ShareController.php
+++ mch/controllers/api/ShareController.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\mch\controllers\api;
 
-use app\mch\forms\api\MchManaInfoPosterForm;
+use app\mch\forms\api\MchSharePosterForm;
 
 /**
  * @deprecated
@@ -16,7 +16,7 @@ class ShareController extends MchMApiController {
      * @throws \yii\db\Exception
      */
     public function actionPoster(){
-        $shareForm = new MchManaInfoPosterForm([
+        $shareForm = new MchSharePosterForm([
             'name'   => $this->mchData['store']['name']
         ]);
         $shareForm->attributes = $this->requestData;

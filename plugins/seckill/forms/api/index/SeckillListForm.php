a33151316c86330fbc64b4c7c9bbf531d3e0259c
wmc <205176371@qq.com>
2021-11-05 11:01:18 +0800

    1

--- plugins/seckill/forms/api/index/SeckillListForm.php
+++ plugins/seckill/forms/api/index/SeckillListForm.php
@@ -77,13 +77,13 @@ class SeckillListForm extends BaseModel
                     } else {
                         $item['falseNum'] = 0;
                     }
+                    $seckill['surplus_percentage'] = sprintf("%.2f",$seckill['falseNum'] / $item['virtual_stock']);
+                    if ($seckill['surplus_percentage'] <= 0) {
+                        $seckill['surplus_percentage'] = 0.01;
+                    }
                 }
                 $seckill['start_time'] = date('Y-m-d', $seckill['start_time']);
                 $seckill['end_time'] = date('Y-m-d H:i:s', $seckill['end_time']);
-                $seckill['surplus_percentage'] = sprintf("%.2f",$seckill['falseNum'] / $item['virtual_stock']);
-                if ($seckill['surplus_percentage'] <= 0) {
-                    $seckill['surplus_percentage'] = 0.01;
-                }
                 $result = $seckill;
             }
 

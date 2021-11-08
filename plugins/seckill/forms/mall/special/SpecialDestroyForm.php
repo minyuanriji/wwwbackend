5ae4cb907753648cd10bf2a7e6575018bf9ee190
wmc <205176371@qq.com>
2021-11-08 16:12:11 +0800

    秒杀后台-秒杀商品列表有商品时，直接删除秒杀专题，创建秒杀专题时，就会存在上次秒杀专题中的秒杀商品

--- plugins/seckill/forms/mall/special/SpecialDestroyForm.php
+++ plugins/seckill/forms/mall/special/SpecialDestroyForm.php
@@ -9,6 +9,8 @@ use app\models\GoodsService;
 use app\models\User;
 use app\plugins\addcredit\models\AddcreditPlateforms;
 use app\plugins\seckill\models\Seckill;
+use app\plugins\seckill\models\SeckillGoods;
+use app\plugins\seckill\models\SeckillGoodsPrice;
 use function Webmozart\Assert\Tests\StaticAnalysis\float;
 use function Webmozart\Assert\Tests\StaticAnalysis\string;
 
@@ -29,6 +31,7 @@ class SpecialDestroyForm extends BaseModel
         if (!$this->validate()) {
             return $this->responseErrorInfo();
         }
+        $t = \Yii::$app->db->beginTransaction();
         try {
             $seckill = Seckill::findOne($this->id);
             if (!$seckill)
@@ -39,8 +42,18 @@ class SpecialDestroyForm extends BaseModel
             if (!$seckill->save())
                 throw new \Exception($seckill->getErrorMessage());
 
+            $seckillGoodsModels = SeckillGoods::find()->where('seckill_id = ' . $seckill->id)->all();
+            foreach ($seckillGoodsModels as $model) {
+                $model->is_delete = 1;
+                $model->update(false);
+
+                SeckillGoodsPrice::deleteAll('seckill_goods_id = ' . $model->id);
+            }
+
+            $t->commit();
             return $this->returnApiResultData(ApiCode::CODE_SUCCESS, '删除成功');
         } catch (\Exception $e) {
+            $t->rollBack();
             return $this->returnApiResultData(ApiCode::CODE_FAIL, $e->getMessage(), [
                 'line' => $e->getLine()
             ]);

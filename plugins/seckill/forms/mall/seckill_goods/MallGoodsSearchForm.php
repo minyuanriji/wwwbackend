1e73625f93dfe41993b62dd1e7cbcc4675510dca
wmc <205176371@qq.com>
2021-11-12 11:34:05 +0800

    1

--- plugins/seckill/forms/mall/seckill_goods/MallGoodsSearchForm.php
+++ plugins/seckill/forms/mall/seckill_goods/MallGoodsSearchForm.php
@@ -37,6 +37,7 @@ class MallGoodsSearchForm extends BaseModel
                 ['mall_id' => \Yii::$app->mall->id],
                 ['status' => 1],
                 ['is_delete' => 0],
+                ['mch_id' => 0],
 //                ['>', 'forehead_score', 0]
             ])->with(['goodsWarehouse' => function ($query) {
                 $query->select('id,cover_pic,name,original_price');

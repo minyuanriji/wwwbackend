898500aae8ba35c16421fad38a98d4a63a2b846f
林工 <746027209@qq.com>
2021-11-15 16:01:01 +0800

    16.00

--- plugins/mch/forms/api/mana/MchManaOrderGiftPacksListForm.php
+++ plugins/mch/forms/api/mana/MchManaOrderGiftPacksListForm.php
@@ -4,6 +4,7 @@ namespace app\plugins\mch\forms\api\mana;
 
 use app\core\ApiCode;
 use app\models\BaseModel;
+use app\models\clerk\ClerkData;
 use app\models\Goods;
 use app\models\Store;
 use app\models\User;
@@ -11,7 +12,6 @@ use app\plugins\giftpacks\models\Giftpacks;
 use app\plugins\giftpacks\models\GiftpacksItem;
 use app\plugins\giftpacks\models\GiftpacksOrder;
 use app\plugins\giftpacks\models\GiftpacksOrderItem;
-use app\plugins\mch\controllers\api\mana\MchAdminController;
 use app\plugins\mch\models\Mch;
 
 class MchManaOrderGiftPacksListForm extends BaseModel {
@@ -65,13 +65,14 @@ class MchManaOrderGiftPacksListForm extends BaseModel {
             }
 
             $selects = ["go.id as order_id", "go.order_sn", "go.order_price", "go.created_at", "go.updated_at", "go.pay_status",
-                "go.pay_at", "go.pay_price", "goi.max_num", "goi.current_num", "goi.expired_at", "gf.title as pack_name",
+                "go.pay_at", "go.pay_price", "goi.id as order_item_id", "goi.max_num", "goi.current_num", "goi.expired_at", "gf.title as pack_name",
                 "gi.name as item_name", "gi.cover_pic item_pic", "gi.item_price", "g.price as goods_price",
                 "u.nickname", "u.mobile", "u.avatar_url", "u.id as user_id", "m.id as mch_id", "s.id as store_id",
                 "s.name as store_name"
             ];
-            $list = $query->select($selects)->asArray()->page($pagination, 10, $this->page)->all();
+            $list = $query->orderBy("go.id DESC")->select($selects)->asArray()->page($pagination, 10, $this->page)->all();
             if($list){
+                $orderItemIds = [];
                 foreach($list as &$item){
                     $remarkArr = [];
                     $remarkArr[] = $item['max_num'] == 0 ? "不限次数" : "剩余".intval($item['current_num'])."次";
@@ -98,6 +99,25 @@ class MchManaOrderGiftPacksListForm extends BaseModel {
                     }
                     $item['created_at'] = date("Y-m-d H:i:s", $item['created_at']);
                     $item['remarks'] = implode("，", $remarkArr);
+                    $orderItemIds[] = $item['order_item_id'];
+                }
+                //获取核销记录
+                $clerkDatas = [];
+                $rows = ClerkData::find()->andWhere([
+                    "AND",
+                    ["status" => 1],
+                    ["source_type" => "giftpacks_order_item"],
+                    ["IN", "source_id", $orderItemIds]
+                ])->asArray()->all();
+                if($rows){
+                    foreach($rows as $row){
+                        $row['updated_at'] = date("Y-m-d H:i:s", $row['updated_at']);
+                        $row['created_at'] = date("Y-m-d H:i:s", $row['created_at']);
+                        $clerkDatas[$row['source_id']][] = $row;
+                    }
+                }
+                foreach($list as &$item){
+                    $item['clerk_log'] = isset($clerkDatas[$item['order_item_id']]) ? $clerkDatas[$item['order_item_id']] : [];
                 }
             }
 

925d7185b0aa3520629b90d60fc65e7790f8f39e
林工 <746027209@qq.com>
2021-12-01 11:33:04 +0800

    11.33

--- plugins/taolijin/controllers/AuthController.php
+++ plugins/taolijin/controllers/AuthController.php
@@ -13,14 +13,14 @@ class AuthController extends Controller{
             "ali_type" => "ali"
         ]);
         if(!$aliModel || $aliModel->is_delete){
-            throw new \Exception("联盟[ID:{$this->ali_id}]不存在");
+            throw new \Exception("联盟[ID:{$ali_id}]不存在");
         }
 
         $setting = @json_decode($aliModel->settings_data, true);
 
         $hostInfo = \Yii::$app->getRequest()->getHostInfo();
         $hostInfo = "https://dev.mingyuanriji.cn";
-        $redirectUri = "{$hostInfo}/h5/#/ali/taolijin/auth/auth";
+        $redirectUri = "{$hostInfo}/web/index.php?r=plugin/taolijin/auth/ali-auth";
 
         $authUrl  = "https://oauth.m.taobao.com/authorize?response_type=code&client_id=" . $setting['app_key'] . "&redirect_uri={$redirectUri}&state=".uniqid()."&view=wap";
 

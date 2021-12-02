8696310fd32420b641c2faa89d4229fa58e68679
林工 <746027209@qq.com>
2021-12-02 16:05:17 +0800

    16.05

--- commands/WsServerController.php
+++ commands/WsServerController.php
@@ -149,8 +149,8 @@ class WsServerController extends BaseCommandController {
      * @param $param
      */
     public function setClientId($token, $param){
+        $mchClient = MchClient::findOne(["token" => $token]);
         try {
-            $mchClient = MchClient::findOne(["token" => $token]);
             if(!$mchClient){
                 $mchClient = new MchClient([
                     "token"      => $token,
@@ -164,6 +164,7 @@ class WsServerController extends BaseCommandController {
             }
         }catch (\Exception $e){
             $this->commandOut($e->getMessage());
+            $param['ws']->close($mchClient->fd, true);
         }
     }
 

b7f8ac10b0d0fc92d456bf50b9935b8f19b50db2
林工 <746027209@qq.com>
2022-02-22 09:50:53 +0800

    9.50

--- plugins/smart_shop/forms/api/ShopListForm.php
+++ plugins/smart_shop/forms/api/ShopListForm.php
@@ -7,7 +7,12 @@ use app\forms\api\APICacheDataForm;
 use app\forms\api\ICacheForm;
 use app\helpers\PoiHelper;
 use app\models\BaseModel;
+use app\models\Store;
+use app\plugins\mch\models\Mch;
+use app\plugins\shopping_voucher\models\ShoppingVoucherFromStore;
 use app\plugins\smart_shop\components\SmartShop;
+use app\plugins\smart_shop\models\Merchant;
+use app\plugins\smart_shop\models\MerchantFzlist;
 
 class ShopListForm extends BaseModel implements ICacheForm {
 
@@ -53,7 +58,7 @@ class ShopListForm extends BaseModel implements ICacheForm {
 
             $list = $shop->getStoreList($pagination, $selects, $wheres, $this->page, $this->limit);
             $defaultLogo = $this->host_info . "/web/static/header-logo.png";
-            foreach($list as &$item){
+            foreach($list as $key => $item){
                 $item['sales']      = 0;
                 $item['distance']   = -1;
                 if(!empty($this->lat) && !empty($this->lng) && $item['coordinates']){
@@ -61,7 +66,12 @@ class ShopListForm extends BaseModel implements ICacheForm {
                     $item['distance'] = (int)PoiHelper::getDistance($this->lng, $this->lat, $coord[1], $coord[0]);
                 }
                 $item['store_logo'] = !empty($item['store_logo']) ? rtrim($shop->setting['host_url'], "/") . str_replace("\\", "/", $item['store_logo']) : $defaultLogo;
+
+                $list[$key] = $item;
             }
+
+            //设置购物券
+            $this->setShoppingVoucher($list);
 
             $sourceData = $this->returnApiResultData(ApiCode::CODE_SUCCESS, '', [
                     'list'        => $list ? $list : [],
@@ -82,4 +92,46 @@ class ShopListForm extends BaseModel implements ICacheForm {
 
     }
 
+    /**
+     * 设置赠送购物券信息
+     * @param $list
+     */
+    private function setShoppingVoucher(&$list){
+        if(!$list) return;
+        $storeIds = [];
+        foreach($list as $item){
+            $storeIds[] = $item['store_id'];
+        }
+
+        $tmps = MerchantFzlist::find()->alias("mfl")
+            ->innerJoin(["mf" => Merchant::tableName()], "mfl.bsh_mch_id=mf.bsh_mch_id AND mf.is_delete=0")
+            ->innerJoin(["m" => Mch::tableName()], "m.id=mf.bsh_mch_id")
+            ->innerJoin(["s" => Store::tableName()], "s.mch_id=m.id")
+            ->innerJoin(["svfs" => ShoppingVoucherFromStore::tableName()], "svfs.store_id=s.id AND svfs.is_delete=0")
+            ->select(["mfl.ss_store_id", "mfl.bsh_mch_id", "m.transfer_rate", "svfs.give_value as shopping_voucher_give_value"])
+            ->andWhere([
+                "AND",
+                ["mfl.is_delete"   => 0],
+                ["m.is_delete"     => 0],
+                ["m.review_status" => Mch::REVIEW_STATUS_CHECKED],
+                ["IN", "mfl.ss_store_id", $storeIds]
+            ])->asArray()->all();
+        $rows = [];
+        if($tmps){
+            foreach($tmps as $tmp){
+                $rows[$tmp['ss_store_id']] = $tmp;
+            }
+        }
+
+        foreach($list as $key => $item){
+            $item['shopping_voucher_remark'] = "";
+            if(isset($rows[$item['store_id']])){
+                $shoppingVoucherGiveValue = $rows[$item['store_id']]['shopping_voucher_give_value'];
+                if($shoppingVoucherGiveValue){
+                    $item['shopping_voucher_remark'] = "付100送".$shoppingVoucherGiveValue."购物券";
+                }
+            }
+            $list[$key] = $item;
+        }
+    }
 }

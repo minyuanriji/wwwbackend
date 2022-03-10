066d59bcc2920cd56285c47cd2426273875bc09f
林工 <746027209@qq.com>
2022-03-10 16:00:31 +0800

    16.00

--- commands/CronController.php
+++ commands/CronController.php
@@ -9,27 +9,25 @@ class CronController extends BaseCommandController{
     ];
 
     public function actionStart(){
-        if(!empty($tasks)){
-            $pm = new \Swoole\Process\ProcessManager();
-            foreach(static::$handlers as $className){
-                $controller = new $className();
-                foreach($controller->actions() as $actionID => $value){
-                    $taskName = $className . "::" . $actionID;
-                    $pm->add(function (\Swoole\Process\Pool $pool, int $workerId) use($taskName, $actionID, $controller){
-                        try {
-                            require_once(__DIR__ . '/../vendor/autoload.php');
-                            require_once __DIR__ . '/../config/const.php';
-                            new \app\core\ConsoleApplication();
-                            $this->commandOut("启动任务{$taskName}");
-                            $controller->runAction($actionID);
-                        }catch (\Exception $e){
-                            $this->commandOut("任务{$taskName} message:" . $e->getMessage() . " file:" . $e->getFile() . " line:" . $e->getLine());
-                        }
-                    });
-                }
+        $pm = new \Swoole\Process\ProcessManager();
+        foreach(static::$handlers as $className){
+            $controller = new $className();
+            foreach($controller->actions() as $actionID => $value){
+                $taskName = $className . "::" . $actionID;
+                $pm->add(function (\Swoole\Process\Pool $pool, int $workerId) use($taskName, $actionID, $controller){
+                    try {
+                        require_once(__DIR__ . '/../vendor/autoload.php');
+                        require_once __DIR__ . '/../config/const.php';
+                        new \app\core\ConsoleApplication();
+                        $this->commandOut("启动任务{$taskName}");
+                        $controller->runAction($actionID);
+                    }catch (\Exception $e){
+                        $this->commandOut("任务{$taskName} message:" . $e->getMessage() . " file:" . $e->getFile() . " line:" . $e->getLine());
+                    }
+                });
             }
-            $pm->start();
         }
+        $pm->start();
     }
 
 }

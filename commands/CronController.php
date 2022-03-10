f66dd3c907ea98c58210374b95ce075c21bfa8a4
林工 <746027209@qq.com>
2022-03-10 15:58:16 +0800

    15.38

--- commands/CronController.php
+++ commands/CronController.php
@@ -4,18 +4,32 @@ namespace app\commands;
 
 class CronController extends BaseCommandController{
 
-    public static function commands(){
-        return [
-            [
-
-            ]
-        ];
-    }
+    public static $handlers = [
+        ShoppingVoucherSendTaskController::class
+    ];
 
     public function actionStart(){
-        $controllers = static::controllers();
-        $pm = new \Swoole\Process\ProcessManager();
-
+        if(!empty($tasks)){
+            $pm = new \Swoole\Process\ProcessManager();
+            foreach(static::$handlers as $className){
+                $controller = new $className();
+                foreach($controller->actions() as $actionID => $value){
+                    $taskName = $className . "::" . $actionID;
+                    $pm->add(function (\Swoole\Process\Pool $pool, int $workerId) use($taskName, $actionID, $controller){
+                        try {
+                            require_once(__DIR__ . '/../vendor/autoload.php');
+                            require_once __DIR__ . '/../config/const.php';
+                            new \app\core\ConsoleApplication();
+                            $this->commandOut("启动任务{$taskName}");
+                            $controller->runAction($actionID);
+                        }catch (\Exception $e){
+                            $this->commandOut("任务{$taskName} message:" . $e->getMessage() . " file:" . $e->getFile() . " line:" . $e->getLine());
+                        }
+                    });
+                }
+            }
+            $pm->start();
+        }
     }
 
 }

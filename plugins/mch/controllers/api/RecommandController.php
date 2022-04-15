ea64f970cdb7bb7e4b441a054237d002c8488c7f
林工 <746027209@qq.com>
2022-04-15 17:33:01 +0800

    17.32

--- plugins/mch/controllers/api/RecommandController.php
+++ plugins/mch/controllers/api/RecommandController.php
@@ -11,8 +11,6 @@ class RecommandController extends ApiController {
     public function actionMchData(){
         $form = new MchRecommandMchDataForm();
         $form->attributes = $this->requestData;
-        $form->longitude  = ApiController::$commonData['city_data']['longitude'];
-        $form->latitude   = ApiController::$commonData['city_data']['latitude'];
         $form->is_login   = !\Yii::$app->user->isGuest;
         $form->login_uid  = $form->is_login ? \Yii::$app->user->id : 0;
         $form->host_info  = \Yii::$app->request->getHostInfo();

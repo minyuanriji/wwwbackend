6a8c7d6b97a08742feea358bbd1b69e78a04bf77
林工 <746027209@qq.com>
2022-05-08 17:01:27 +0800

    17.01

--- plugins/smart_shop/controllers/api/store_admin/StoreSetController.php
+++ plugins/smart_shop/controllers/api/store_admin/StoreSetController.php
@@ -4,9 +4,14 @@ namespace app\plugins\smart_shop\controllers\api\store_admin;
 
 use app\plugins\smart_shop\controllers\api\AdminAuthController;
 use app\plugins\smart_shop\forms\api\store_admin\StoreSetDetailForm;
+use app\plugins\smart_shop\forms\api\store_admin\StoreSetSaveForm;
 
 class StoreSetController extends AdminAuthController {
 
+    /**
+     * 获取配置
+     * @return \yii\web\Response
+     */
     public function actionIndex(){
         $form = new StoreSetDetailForm();
         $form->attributes  = $this->requestData;
@@ -15,4 +20,15 @@ class StoreSetController extends AdminAuthController {
         return $this->asJson($form->get());
     }
 
+    /**
+     * 保存配置
+     * @return \yii\web\Response
+     */
+    public function actionSave(){
+        $form = new StoreSetSaveForm();
+        $form->attributes  = $this->requestData;
+        $form->merchant_id = $this->merchant ? $this->merchant['id'] : 0;
+        $form->store_id    = $this->store ? $this->store['id'] : '';
+        return $this->asJson($form->save());
+    }
 }
--- plugins/smart_shop/forms/api/store_admin/StoreSetDetailForm.php
+++ plugins/smart_shop/forms/api/store_admin/StoreSetDetailForm.php
@@ -4,6 +4,10 @@ namespace app\plugins\smart_shop\forms\api\store_admin;
 
 use app\core\ApiCode;
 use app\models\BaseModel;
+use app\plugins\mch\models\Mch;
+use app\plugins\smart_shop\models\Merchant;
+use app\plugins\smart_shop\models\MerchantFzlist;
+use app\plugins\smart_shop\models\StoreSet;
 
 class StoreSetDetailForm extends BaseModel{
 
@@ -24,10 +28,36 @@ class StoreSetDetailForm extends BaseModel{
 
         try {
 
-            
+            $row = Merchant::find()->alias("mf")
+                ->innerJoin(["mfl" => MerchantFzlist::tableName()], "mfl.bsh_mch_id=mf.bsh_mch_id")
+                ->innerJoin(["m" => Mch::tableName()], "m.id=mf.bsh_mch_id")
+                ->where([
+                    "m.review_status" => Mch::REVIEW_STATUS_CHECKED,
+                    "m.is_delete"     => 0,
+                    "mf.is_delete"    => 0,
+                    "mfl.is_delete"   => 0,
+                    "mfl.ss_store_id" => $this->store_id
+                ])->asArray()->select(["mf.bsh_mch_id"])->one();
+
+            if(!$row){
+                throw new \Exception("请先入驻补商汇平台");
+            }
+
+            $set = StoreSet::findOne([
+                "bsh_mch_id"  => $row['bsh_mch_id'],
+                "ss_mch_id"   => $this->merchant_id,
+                "ss_store_id" => $this->store_id
+            ]);
+            if(!$set){
+                $set = [
+                    "transfer_rate" => ''
+                ];
+            }else{
+                $set = $set->getAttributes();
+            }
 
             return $this->returnApiResultData(ApiCode::CODE_SUCCESS, null, [
-
+                "setting" => $set
             ]);
         }catch (\Exception $e){
             return $this->returnApiResultData(ApiCode::CODE_FAIL, $e->getMessage());

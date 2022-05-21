ed3bc668d401eca5c2671e11cf816a76f81cfb42
林工 <746027209@qq.com>
2022-05-21 10:57:15 +0800

    10.57

--- plugins/smart_shop/forms/api/store_admin/AccountAccountLogForm.php
+++ plugins/smart_shop/forms/api/store_admin/AccountAccountLogForm.php
@@ -5,6 +5,7 @@ namespace app\plugins\smart_shop\forms\api\store_admin;
 use app\core\ApiCode;
 use app\models\BaseModel;
 use app\models\User;
+use app\plugins\smart_shop\models\Cyorder;
 use app\plugins\smart_shop\models\StoreAccountLog;
 use app\plugins\smart_shop\models\StorePayOrder;
 
@@ -46,19 +47,31 @@ class AccountAccountLogForm extends BaseModel{
                 $payTypes = ['alipay' => '支付宝', 'wechat' => '微信', 'balance' => '余额'];
                 foreach($list as $key => $row){
                     $row['created_at'] = date("Y-m-d H:i:s", $row['created_at']);
-                    if($row['source_type'] == "store_pay_order"){
+                    if($row['source_type'] == "store_pay_order") {
                         $info = StorePayOrder::find()->alias("o")
                             ->select(["o.order_no", "o.order_price", "o.pay_type", "o.pay_time", "u.nickname", "u.mobile"])
                             ->where(["o.id" => $row['source_id']])
                             ->leftJoin(["u" => User::tableName()], "u.id=o.pay_uid")
                             ->asArray()
                             ->one();
-                        if($info){
+                        if ($info) {
                             $info['pay_type'] = isset($payTypes[$info['pay_type']]) ? $payTypes[$info['pay_type']] : "";
                         }
                         $row['source_info'] = $info;
+                    }elseif($row['source_type'] == "cyorder"){
+                        $cyorder = Cyorder::findOne($row['source_id']);
+                        if($cyorder){
+                            $row['source_info'] = [
+                                'pay_price' => $cyorder->pay_price,
+                                'pay_user_mobile' => $cyorder->pay_user_mobile,
+                                'shopping_voucher' => $cyorder->shopping_voucher_info ? json_decode($cyorder->shopping_voucher_info, true) : '',
+                                'score' => $cyorder->score_info ? json_decode($cyorder->score_info, true) : '',
+                            ];
+                        }else{
+                            $row['source_info'] = '';
+                        }
                     }else{
-                        $row['source_type'] = '';
+                        $row['source_info'] = '';
                     }
                     $list[$key] = $row;
                 }

9bffd510bd01e4cd4dd8b54575eb5d5f9f1806d7
林工 <746027209@qq.com>
2022-05-31 18:30:01 +0800

    18.29

--- plugins/perform_distribution/models/PerformDistributionGoods.php
+++ plugins/perform_distribution/models/PerformDistributionGoods.php
@@ -61,13 +61,23 @@ class PerformDistributionGoods extends BaseActiveRecord
             return $awardInfo;
         }
 
+        //记录商品名称
+        $awardInfo['goods_name'] = $goods->goodsWarehouse->name;
+
         //获取同区域的所有上级
         $awardParentUser = PerformDistributionUser::findOne([
             "user_id" => $payUser->parent_id,
             "mall_id" => $payUser->mall_id
         ]);
+
+        //区域信息
+        $region = PerformDistributionRegion::findOne([
+            "mall_id" => $payUser->mall_id,
+            "id"      => $awardParentUser ? $awardParentUser->region_id : 0
+        ]);
+
         $parentReLink = UserRelationshipLink::findOne(["user_id" => $payUser->parent_id]);
-        if($awardParentUser && $parentReLink){
+        if($region && $awardParentUser && $parentReLink){
             $rows = UserRelationshipLink::find()->alias("url")
                 ->innerJoin(["u" => User::tableName()], "u.id=url.user_id")
                 ->innerJoin(["pdu" => PerformDistributionUser::tableName()], "pdu.user_id=url.user_id AND pdu.is_delete=0")
@@ -88,17 +98,20 @@ class PerformDistributionGoods extends BaseActiveRecord
                     }
                 }
             }
-        }
 
-        //计算每个奖励人员可获得的奖励
-        $profitPrice = floatval($goods->profit_price) * intval($orderDetail->num); //商品利润
-        $awardInfo['profit_price'] = $profitPrice;
-        foreach($awardInfo['award_users'] as $key => $awardUser){
-            if($awardInfo['award_type'] == 0){ //按比例
-                $awardInfo['award_users'][$key]['price'] = $profitPrice * floatval($awardUser['rule']['value']/100);
-            }else{ //按固定值
-                $awardInfo['award_users'][$key]['price'] = min($awardUser['rule']['value'], $profitPrice);
-                $profitPrice -= $awardInfo['award_users'][$key]['price'];
+            //记录区域名称
+            $awardInfo['region_name'] = $region->name;
+
+            //计算每个奖励人员可获得的奖励
+            $profitPrice = floatval($goods->profit_price) * intval($orderDetail->num); //商品利润
+            $awardInfo['profit_price'] = $profitPrice;
+            foreach($awardInfo['award_users'] as $key => $awardUser){
+                if($awardInfo['award_type'] == 0){ //按比例
+                    $awardInfo['award_users'][$key]['price'] = $profitPrice * floatval($awardUser['rule']['value']/100);
+                }else{ //按固定值
+                    $awardInfo['award_users'][$key]['price'] = min($awardUser['rule']['value'], $profitPrice);
+                    $profitPrice -= $awardInfo['award_users'][$key]['price'];
+                }
             }
         }
 
